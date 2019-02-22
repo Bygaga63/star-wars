ce9f8c2dab629400658416ec0734cba4e29e8e56
Dmitry <bygaga63@gmail.com>
2019-02-22 22:10:41 +0300

    Updates

---
+++ precache-manifest.355c18094f0157a6d7cf24cdccd7bd1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "38f261eaf4167497d02c",
+    "url": "/static/js/main.38f261ea.chunk.js"
+  },
+  {
+    "revision": "759edc7d5cc36001aa9b",
+    "url": "/static/js/2.759edc7d.chunk.js"
+  },
+  {
+    "revision": "38f261eaf4167497d02c",
+    "url": "/static/css/main.0ebac208.chunk.css"
+  },
+  {
+    "revision": "aa24ce2ac37cc53f95edb035f5e6ab1c",
+    "url": "/index.html"
+  }
+];
